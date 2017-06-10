f8a1a01a3a23d67149cfb74bc23a7a230fd3f321
ben8406091 <ycps60714@gmail.com>
2017-06-10 14:54:07 +0800

    下午更新

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,20 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', function () {
+    $name = 'tad';
+    $say  = '嗨！';
+    $date = date("Y年m月d日");
+
+    return view('welcome', compact('name', 'say', 'date'));
+
 });
